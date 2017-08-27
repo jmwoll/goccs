7514cec7e1018f707a8f18d8613e375e88c5df9a
Jan Wollschlaeger <jm.wollschlaeger@gmail.com>
2017-08-27 15:17:21 +0200

    parameters need to be refactored into their own file

--- ehsccs.go
+++ ehsccs.go
@@ -1,1 +1,66 @@
 package main
+
+// Copyright (C) 2017  Jan Wollschläger <jmw.tau@gmail.com>
+// This file is part of goccs.
+//
+// goccs is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+type vec3 struct {
+    x float64
+    y float64
+    z float64
+}
+
+type line struct {
+    origin float64
+    direction vec3
+}
+
+type sphere struct {
+    radius float64
+    center vec3
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
