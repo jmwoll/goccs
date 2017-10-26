8fd20ca1136c514b0f435a5c9c0e211ae1da2181
Jan Wollschlaeger <jm.wollschlaeger@gmail.com>
2017-10-26 21:51:36 +0200

    Added file ending .exe to windows binaries.

--- build_cross_platform.sh
+++ build_cross_platform.sh
@@ -1,7 +1,6 @@
 GOOS=linux GOARCH=386 go build -o bin/goccs_linux_386
 GOOS=linux GOARCH=amd64 go build -o bin/goccs_linux_amd64
 GOOS=freebsd GOARCH=386 go build -o bin/goccs_freebsd_386
-GOOS=windows GOARCH=386 go build -o bin/goccs_windows_386
-GOOS=windows GOARCH=amd64 go build -o bin/goccs_windows_amd64
+GOOS=windows GOARCH=386 go build -o bin/goccs_windows_386.exe
+GOOS=windows GOARCH=amd64 go build -o bin/goccs_windows_amd64.exe
 GOOS=darwin GOARCH=386 go build -o bin/goccs_darwin_386
-
